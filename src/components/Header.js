eaef9b85a46467de492729848259d834e355b3d1
Marcello Silvério <smarcelloc@gmail.com>
2021-01-05 18:06:57 -0300

    Implementado TopBar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,42 @@
-import { Button } from "@material-ui/core";
+import { AppBar, Button, IconButton, Toolbar, Avatar } from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
+import {
+  NotificationsNone as NotificationsNoneIcon
+} from '@material-ui/icons';
+
+const useStyle = makeStyles((theme) => ({
+
+  appBar: {
+    boxShadow: '0 .125rem .25rem rgba(0,0,0,.075)',
+  },
+
+  logo: {
+    maxHeight: 55
+  },
+
+  separator: {
+    flexGrow: 1
+  }
+
+}));
+
 
 function Header() {
+
+  const classes = useStyle();
+
   return (
-    <header className="header">
-      <div className="toolbar">
-        <div>
-          <h1>ConectaDev</h1>
-        </div>
-        <div>
-          <Button variant="contained" color="primary">Novo posts</Button>
-          <span>img 01</span>
-          <span>img 02</span>
-        </div>
-      </div>
-    </header>
+    <AppBar position="fixed" color="inherit" className={classes.appBar}>
+      <Toolbar>
+        <img src="/images/logo.png" alt="Logotipo do ConectaDev" className={classes.logo} />
+        <div className={classes.separator}></div>
+        <Button variant="contained" color="primary">Novo posts</Button>
+        <IconButton title="Notificação">
+          <NotificationsNoneIcon />
+        </IconButton>
+        <Avatar src="/images/avatars/default.png" alt="Perfil do sistema" />
+      </Toolbar>
+    </AppBar>
   );
 }
 
