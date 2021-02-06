70ad9f40bed027c7a6e827fde3219cebe5ddeda7
Marcello Silvério <smarcelloc@gmail.com>
2021-02-05 21:08:25 -0300

    Criação do Feed e PostCard

--- src/Home/components/Feed.js
+++ src/Home/components/Feed.js
@@ -1,16 +1,53 @@
 import { makeStyles } from "@material-ui/styles";
+import PostCard from '../../components/PostCard';
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      id: 1,
+      name: 'Rodrigo Neves',
+      username: 'rodneves',
+      avatar: '/images/avatars/default.png'
+    },
+    title: "Criando um App do zero utilizando React.js",
+    date: "April 7, 2020",
+    description: "Fala pessoal! QUal o framework favorito de vcs?",
+    hashtags: ['dotnet', 'javascript', 'reactjs', 'developer'],
+    image: "/images/posts/post1.png"
+  },
+  {
+    id: 2,
+    author: {
+      id: 1,
+      name: 'Rodrigo Neves',
+      username: 'rodneves',
+      avatar: '/images/avatars/default.png'
+    },
+    title: "Comparativo entre React.js e Vue.js - Performance",
+    date: "April 1, 2020",
+    description: "Quero criar um bootcamp gratuito para passar um pouco da minha experiência",
+    hashtags: ['dotnet', 'javascript', 'reactjs', 'developer'],
+    image: '/images/posts/post2.png'
+
+  }
+];
 
 const useStyle = makeStyles((theme) => ({
-
+  root: {
+  }
 
 }));
-
 
 function Feed() {
   const classes = useStyle();
 
   return (
-    <div>Feed</div>
+    <div className={classes.root}>
+      {posts.map(post => (
+        <PostCard key={post.id} post={post} />
+      ))}
+    </div>
   );
 }
 
---
+++ src/components/PostCard.js
@@ -0,0 +1,119 @@
+import { makeStyles } from "@material-ui/styles";
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import { Avatar, CardActionArea, Typography, IconButton } from "@material-ui/core";
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import BookmarkIcon from '@material-ui/icons/Bookmark';
+import MessageIcon from '@material-ui/icons/Message';
+
+
+const useStyle = makeStyles((theme) => ({
+    root: {
+        marginBottom: theme.spacing(2),
+    },
+
+    subheader: {
+        display: 'flex',
+        alignItems: 'center',
+    },
+
+    caption: {
+        marginRight: theme.spacing(1)
+    },
+
+    hashtag: {
+        marginLeft: theme.spacing(2),
+    },
+
+    message: {
+        height: 'auto',
+        marginBottom: theme.spacing(2),
+        padding: '0 24px'
+    },
+
+    image: {
+        height: 300,
+        width: '100%',
+        maxWidth: 1200
+    },
+
+    content: {
+        padding: 0
+    },
+    separator: {
+        margin: 'auto'
+    }
+
+}));
+
+function PostCard({ post }) {
+    const classes = useStyle();
+    return (
+        <Card className={classes.root}>
+            <CardHeader
+                title={
+                    <Typography variant="h6">
+                        {post.title}
+                    </Typography>
+                }
+
+                avatar={
+                    <Avatar src={post.author.avatar} />
+                }
+
+                subheader={
+                    <div className={classes.subheader}>
+                        <Typography variant="caption" className={classes.caption}>
+                            Avaliado por
+                        </Typography>
+                        <Typography variant="subtitle2" className={classes.caption}>
+                            {post.author.name}
+                        </Typography>
+                        <Typography variant="subtitle2" className={classes.caption}>
+                            {post.date}
+                        </Typography>
+                    </div>
+                }
+            />
+
+            <CardContent className={classes.content}>
+                <Typography
+                    variant="body1"
+                    style={{
+                        marginBottom: 8
+                    }}
+                >
+                    {post.hashtags.map(hashtag => (
+                        <span className={classes.hashtag}>#{hashtag}</span>
+                    ))}
+                </Typography>
+
+                <CardActionArea>
+                    <img src={post.image} className={classes.image} />
+                </CardActionArea>
+
+            </CardContent>
+
+            <CardActions disableSpacing>
+                <IconButton aria-label="add to favorites">
+                    <FavoriteIcon />
+                    <Typography color="textSecondary" variant="body2">10</Typography>
+                </IconButton>
+                <IconButton aria-label="add to message">
+                    <MessageIcon />
+                    <Typography color="textSecondary" variant="body2">10</Typography>
+                </IconButton>
+
+                <div className={classes.separator}></div>
+
+                <IconButton aria-label="bookmark of feed">
+                    <BookmarkIcon />
+                </IconButton>
+            </CardActions>
+        </Card>
+    );
+}
+
+export default PostCard;
